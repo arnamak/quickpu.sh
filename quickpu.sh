b36bcaa4b6e8568c81e75425760b721929de4ee3
arnamak <mak.arnautovic@dnsimple.com>
2016-02-26 00:50:51 -0600

    formatting

--- quickpu.sh
+++ quickpu.sh
@@ -10,7 +10,7 @@ if [ -e ./quickpu.sh ]; then
 fi
 
 # This sets a 'qp' alias for quickpu.sh, if you don't already have an alias for 'qp'. If you do, you'll have to manually create an alias for quickpu.sh should you want to have one.
-if [ "$(type qp)" = "qp not found" ]; then
+if [ "$( type qp )" = "qp not found" ]; then
     "alias qp='"$( echo $HOME/$SCRIPTS_DIR )"/quickpu.sh'" >> ~/.zshrc
 fi
 
