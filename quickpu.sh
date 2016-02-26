91d6308770ee954f699687616d2d1eb8f4eeae18
arnamak <mak.arnautovic@dnsimple.com>
2016-02-26 15:25:00 -0600

    Only execute bare script during first-run if repo
    The bare script will only work in a git repository
    If you install it somewhere not-in a git repo,
    all of the setup will be performed but it won't run
    automatically.

--- quickpu.sh
+++ quickpu.sh
@@ -12,12 +12,20 @@ echo "`echo git status`\ngit add .\necho \"Enter your commit message followed by
 
 # This sets a 'qp' alias for quickpu.sh, if you don't already have an alias for 'qp'.
 # If you do, you'll have to manually create an alias for quickpu.sh should you want to have one.
-# Remember to source your .zshrc once the alias has been created.
+# If you use a different shell simply change the two following instances of '~/.zshrc' to 
+# whatever you want.
 if [ "$( grep -r "alias qp" ~/.zshrc )" ]; then
     echo alias already created
 else
     echo "alias qp='sh "`echo $HOME/$SCRIPTS_DIR`"/quickpu.sh'" >> ~/.zshrc
 fi
+# Remember to source your .zshrc once the alias has been created.
 
-sh "$HOME/$SCRIPTS_DIR"/quickpu.sh
-rm "$(pwd)"/quickpu.sh
+# If first run is performed in a git repository, the bare script will run.
+if [ -d .git ]; then
+    sh "$HOME/$SCRIPTS_DIR"/quickpu.sh
+fi
+
+# Erases the first-run version of quickpu.sh and leaves only the bare script
+current="$0"
+shred -u ${current}
